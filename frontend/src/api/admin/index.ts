50410734f96df8af44688a63fdfecb10055ac24c
shaw <shaw-wei@foxmail.com>
2026-06-10 14:16:51 +0800

    Add admin compliance acknowledgement gate

--- frontend/src/api/admin/index.ts
+++ frontend/src/api/admin/index.ts
@@ -32,6 +32,7 @@ import adminPaymentAPI from './payment'
 import affiliatesAPI from './affiliates'
 import riskControlAPI from './riskControl'
 import lotteryAPI from './lottery'
+import adminComplianceAPI from './compliance'
 
 /**
  * Unified admin API object for convenient access
@@ -65,7 +66,8 @@ export const adminAPI = {
   payment: adminPaymentAPI,
   affiliates: affiliatesAPI,
   riskControl: riskControlAPI,
-  lottery: lotteryAPI
+  lottery: lotteryAPI,
+  compliance: adminComplianceAPI
 }
 
 export {
@@ -97,7 +99,8 @@ export {
   adminPaymentAPI,
   affiliatesAPI,
   riskControlAPI,
-  lotteryAPI
+  lotteryAPI,
+  adminComplianceAPI
 }
 
 export default adminAPI
