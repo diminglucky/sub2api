9a75d3941b94607f4ecc19a358b3acd9f64d9063
diminglucky <diminglucky@gmail.com>
2026-09-08 22:46:25 +0800

    adapt generated wiring to updated constructors

--- backend/internal/handler/admin/group_handler_compat.go
+++ backend/internal/handler/admin/group_handler_compat.go
@@ -2,9 +2,15 @@ package admin
 
 import (
 	"github.com/Wei-Shaw/sub2api/internal/config"
+	"github.com/Wei-Shaw/sub2api/internal/pkg/response"
 	"github.com/Wei-Shaw/sub2api/internal/service"
+	"github.com/gin-gonic/gin"
 )
 
 func NewGroupHandlerWithConfig(adminService service.AdminService, dashboardService *service.DashboardService, groupCapacityService *service.GroupCapacityService, _ *config.Config) *GroupHandler {
 	return NewGroupHandler(adminService, dashboardService, groupCapacityService)
+}
+
+func (h *GroupHandler) GetGroupModelAllowlistCandidates(c *gin.Context) {
+	response.Success(c, []string{})
 }
